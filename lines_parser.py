9a1bb40301c14e0bb4b775829fdebc92ad850331
Krzysztof Kielak <krzysztof.kielak@gmail.com>
2020-01-11 13:34:38 +0100

    lines parser for the output of the go based generator

---
+++ lines_parser.py
@@ -0,0 +1,55 @@
+import struct
+
+
+class Frame:
+    def __init__(self, frame_number):
+        self.frame_number = frame_number
+        self.lines = []
+        self.screen_width = 320
+        self.screen_height = 256
+
+    def add_line(self, x1, y1, x2, y2):
+        self.lines.append((x1, y1, x2, y2))
+
+    def add_line_str(self, data):
+        (x1, y1, x2, y2) = map((lambda x: int(x)), data.split(','))
+        self.add_line(x1, y1, x2, y2)
+
+    def export(self):
+        result = struct.pack('>H', len(self.lines))
+        for (x1, y1, x2, y2) in self.lines:
+            address = int(y1 * self.screen_width / 2) + int(x1 / 16)
+            result += struct.pack(">HHhh", address, x1, x2 - x1, y2 - y1)
+            # print("addr: %04x | x1: %02x | x2-x1: %02x | y2-y1: %02x" % (address, x1, x2 - x1, y2 - y1))
+        return result
+
+
+class Parser:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.frames = []
+
+    def parse(self):
+        current_frame = None
+        with open(self.file_name) as f:
+            lines = f.readlines()
+            for lin in lines:
+                (element, data) = lin.rstrip().lower().split(':')
+                if element.startswith("frame"):
+                    current_frame = Frame(int(data))
+                    self.frames.append(current_frame)
+                elif element.startswith("line"):
+                    current_frame.add_line_str(data)
+
+    def export(self, export_file_name):
+        with open(export_file_name, mode="wb") as f:
+            f.write(struct.pack(">H", len(self.frames)))
+            for frame in self.frames:
+                bin_frame = frame.export()
+                f.write(bin_frame)
+
+
+if __name__ == '__main__':
+    p = Parser("lines.txt")
+    p.parse()
+    p.export("test.dat")
